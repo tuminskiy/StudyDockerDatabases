cdfacd1c86f39fc88a8705f1243a4986c9beada1
tuminskiy <unknowuzvername@gmail.com>
2021-10-25 15:22:31 +0300

    Added filling elasticsearch db

---
+++ py/fill_elastic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.8
+
+from elasticsearch import Elasticsearch
+
+HOST = "172.17.0.6"
+INDEX = "tumindb"
+
+es = Elasticsearch(host=HOST)
+
+if es.indices.exists(index=INDEX):
+  es.indices.delete(index=INDEX)
+
+es.indices.create(index=INDEX)
+
+for i in range(1, 101):
+  n = 8 if i % 2 == 0 else 16
+
+  data = {
+    "lesson": "lesson No. " + str(i),
+    "discription": "description No. " + str(i),
+    "n_lecture": n,
+    "n_practice": n 
+  }
+
+  es.index(index=INDEX, document=data)
+
